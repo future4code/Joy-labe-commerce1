17a53019e34caa850b08dd06b9f960b93613d3a4
RickHardBR <rickhard@bol.com.br>
2021-11-30 20:53:46 -0300

    mudanças nos arquivos app.js cardprodutos e produtos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Carrinho from './components/carrinho';
-import Produtos from './components/produtos';
-import Filtro from './components/filtro';
+import { Carrinho } from './components/carrinho';
+import { Produtos } from './components/produtos';
+import { Filtro } from './components/filtro';
 import styled from 'styled-components';
 
 //import styled from 'styled-components';
@@ -13,95 +13,130 @@ const AppCont = styled.div`
   gap: 8px;
 `;
 
-const produtos = [      {
-  id: 1,
-  name: "Foguete da Missão Apollo 11",
-  value: 10000.0,
-  imageUrl: "https://picsum.photos/200/200",
-},
-{
-  id: 2,
-  name: "Foguete da Missão Apollo 11",
-  value: 10000.0,
-  imageUrl: "https://picsum.photos/200/200",
-},{
-  id: 3,
-  name: "Foguete da Missão Apollo 11",
-  value: 10000.0,
-  imageUrl: "https://picsum.photos/200/200",
-},
-{
-  id: 4,
-  name: "Foguete da Missão Apollo 11",
-  value: 10000.0,
-  imageUrl: "https://picsum.photos/200/200",
-},
-{
-  id: 5,
-  name: "Foguete da Missão Apollo 11",
-  value: 10000.0,
-  imageUrl: "https://picsum.photos/200/200",
-},{
-  id: 6,
-  name: "Foguete da Missão Apollo 11",
-  value: 10000.0,
-  imageUrl: "https://picsum.photos/200/200",
-}
-
-
+const produtos = [
+  {
+    id: 1,
+    nome: 'Falcon 9',
+    preco: 100.00,
+    imagem: 'https://engenhariaaeronautica.com.br/wp-content/uploads/2020/07/estrutura-falcon9-2-768x768.jpg'
+  },
+  {
+    id: 2,
+    nome: 'Starship',
+    preco: 200.00,
+    imagem: 'https://tecnoblog.net/meiobit/wp-content/uploads/2019/09/20190930efvbpwuxuaauy5w.jpg'
+  },
+  {
+    id: 3,
+    nome: 'Super Heavy',
+    preco: 300.00,
+    imagem: 'https://i.redd.it/2rx75mrsez821.png'
+  },
+  {
+    id: 4,
+    nome: 'Blue Origin',
+    preco: 400.00,
+    imagem: 'https://img.ibxk.com.br/2021/08/26/26114401962259.jpg'
+  },
+  {
+    id: 5,
+    nome: 'SpaceX SN15',
+    preco: 350.00,
+    imagem: 'https://1734811051.rsc.cdn77.org/data/images/full/372411/spacex-makes-way-for-its-sn8-prototype-by-purposely-destroying-starship-tank.png'
+  },
+  {
+    id: 6,
+    nome: 'Saturn V',
+    preco: 150.00,
+    imagem: 'https://static.turbosquid.com/Preview/2019/10/27__11_34_00/NASA1.pngB5471682-02E0-42CC-83F4-BA2BBD0BE03AZoom.jpg'
+  }
 ]
 
 
 export default class App extends React.Component {
-// estado
-state = {
+  // estado
+  state = {
     valorMaximo: "",
     valorMinimo: "",
     buscaPorNome: "",
-    lista: [
-      {
-        id: 1,
-        name: "Foguete da Missão Apollo 11",
-        value: 10000.0,
-        imageUrl: "https://picsum.photos/200/200",
-      },
-      {
-        id: 2,
-        name: "Foguete da Missão Apollo 11",
-        value: 10000.0,
-        imageUrl: "https://picsum.photos/200/200",
-      },{
-        id: 3,
-        name: "Foguete da Missão Apollo 11",
-        value: 10000.0,
-        imageUrl: "https://picsum.photos/200/200",
-      }
-    ]
+    cestaDeProdutos: [],
   };
 
 
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (prevState.inputValue !== this.state.cestaDeProdutos)
+  //     localStorage.setItem("cesta", this.state.cestaDeProdutos)
+  //   console.log("cestaDeProdutos salva")
 
+  //   if (prevState.filtro !== this.state.cestaDeProdutos)
+  //     localStorage.setItem("cesta", this.state.cestaDeProdutos)
+  //   console.log("status cesta")
 
-render(){
-// filtros e map
+  //   if (prevState.tarefas !== this.state.cestaDeProdutos)
+  //     localStorage.setItem("cesta", this.state.cestaDeProdutos.id)
+  //   console.log("array")
 
-  return (
+  //   if (prevState.tarefas !== this.state.cestaDeProdutos)
+  //     localStorage.setItem("cesta", this.state.cestaDeProdutos.nome)
+  //   console.log("array")
 
-<AppCont>
-    <div>
-      
-      E - Commerce Rocket Star
-    <Filtro />
-    </div>
-    <div>
-    <Produtos />
-    </div>
-    <div>
-    <Carrinho />
+  //   if (prevState.tarefas !== this.state.cestaDeProdutos)
+  //     localStorage.setItem("tarefa", this.state.cestaDeProdutos.preco)
+  //   console.log("array")
+  // };
 
-    </div>
-    </AppCont>
-  );
-}
+  // //enviar JSON quando adicionar carrinho
+  // componentDidMount() {
+  //   this.adicionaAoCarrinho();
+  //   const idLS = localStorage.getItem("id") || "";
+  //   const nomeLS = localStorage.getItem("nome") || "";
+  //   const precoLS = localStorage.getItem("preco") || "";
+  //   this.setState({ id: idLS, nome: nomeLS, preco: precoLS });
+  // }
+
+  adicionaAoCarrinho = (props) => {
+    const AdicionaProduto = {
+      id: this.state.cestaDeProdutos,
+      nome: this.state.cestaDeProdutos,
+      preco: this.state.cestaDeProdutos
+    }
+    const novaCesta = [...this.state.cestaDeProdutos, AdicionaProduto]
+    this.setState({ cestaDeProdutos: novaCesta })
+    console.log("ok produto encestado!!")
+  //   localStorage.setItem("Cesta-De-Produtos", JSON.stringify(novaCesta));
+   }
+  removeDoCarrinho = id => {
+
+    // tentativa de apagar carrinho
+
+    const novaCesta = this.state.cestaDeProdutos.filter(nome => {
+      return id !== nome.id;
+    });
+
+    this.setState({ cestaDeProdutos: novaCesta });
+  };
+
+  render() {
+    // filtros e map
+    
+    return (
+
+      <AppCont>
+        <div>
+          <header>
+        E - Commerce Rocket Star
+          </header>
+          <Filtro />
+        </div>
+        <div>
+          <Produtos />
+        </div>
+        <div>
+          <Carrinho />
+     
+        </div>
+      </AppCont>
+    );
+  }
 
 }
--- src/components/CardProduto.jsx
+++ src/components/CardProduto.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
+
 const ProdutoContainer = styled.div`
   border: 1px solid black;
   display: flex;
@@ -17,35 +18,28 @@ const ItensProduto = styled.div`
   }
 `
 
-const PhotoProduto = styled.img`
-  width: 100%;
-`
+// const PhotoProduto = styled.img`
+//   width: 100%;
+// `
 const CardDoBotao = styled.button`
   align-self: center;
   margin-top: 4px;
 `
 
 export default class CardProduto extends React.Component {
-
-
-render(){
 // filtros e map
 
-  return (
-
-
-    <div>
-        <ProdutoContainer>
-          <PhotoProduto src='https://picsum.photos/1000/740' alt='Imagem do produto' />
-          <ItensProduto>
-          <p>Produto 1</p>
-          <p>R$ 100,00</p>
-          <CardDoBotao>
-         Adicionar ao carrinho
-         </CardDoBotao>
-         </ItensProduto>
-        </ProdutoContainer>
-    </div>
-  );
-}
+  render() {
+    const produtos = this.props.produtos
+    return <ProdutoContainer>
+      <img src={this.props.imagem}/>
+      <ItensProduto>
+        <p>{this.props.nome}</p>
+        <p>R${this.props.preco},00</p>
+        <CardDoBotao onClick={() => this.props.adicionaAoCarrinho(this.props.id)}>
+          Adicionar ao carrinho
+        </CardDoBotao>
+      </ItensProduto>
+    </ProdutoContainer>
+  }
 }
