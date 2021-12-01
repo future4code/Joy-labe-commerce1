23dc7925ea26e3bd0ab13ba7d282f38ab9d7a5c3
Athos de Oliveira <64941377+athosoli@users.noreply.github.com>
2021-11-30 22:14:45 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -59,8 +59,7 @@ export default class App extends React.Component {
     valorMaximo: "",
     valorMinimo: "",
     buscaPorNome: "",
-    cestaDeProdutos: [],
-    ]
+    cestaDeProdutos: []
   };
 
 
