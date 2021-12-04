4c7f9672b2504a1fb9c29e09ef3f3d3236546c0b
Athos de Oliveira <64941377+athosoli@users.noreply.github.com>
2021-12-03 21:04:05 -0300

    correcao

--- src/components/CardProdutos/CardProduto.jsx
+++ src/components/CardProdutos/CardProduto.jsx
@@ -4,12 +4,12 @@ import { ProdutoContainer, ItensProduto, PhotoProduto, CardDoBotao } from './sty
 
 export function Itens(props) {
   return <ProdutoContainer>
-          <PhotoProduto img src={props.naves.imagem}>
+          <PhotoProduto img src={props.nave.imagem}>
           </PhotoProduto>
           <ItensProduto>
-          <p>{props.naves.nome}</p>
-          <p>R$ {props.naves.preco.toFixed(2).replace(".", ",")}</p>
-          <CardDoBotao onClick={() => this.props.adicionaAoCarrinho(props.naves.id)}>
+          <p>{props.nave.nome}</p>
+          <p>R$ {props.nave.preco.toFixed(2).replace(".", ",")}</p>
+          <CardDoBotao onClick={() => this.props.adicionaAoCarrinho(props.nave.id)}>
           Adicionar ao carrinho
           </CardDoBotao>
           </ItensProduto>
--- src/components/carrinho.jsx
+++ src/components/carrinho.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import naves from "../App"
+// import naves from "../App"
+import { Itens } from './CardProdutos/CardProduto';
 
 const ContainerCarrinho = styled.div`
 border-radius:10px;
@@ -14,29 +15,20 @@ export class Carrinho extends React.Component {
 
   render() {
 
-    const navesList = naves.map((item)=>{
-      return(
-      <p key={item.name} onClick={() => this.adicionaAoCarrinho(item)}>
-      {item.nome}, {item.preco}</p>
-      )})
+    // const navesList = naves.map((item)=>{
+    //   return(
+    //   <p key={item.name} onClick={() => this.adicionaAoCarrinho()}>
+    //   {item.nome}, {item.preco}</p>
+    //   )})
 
     return (
       <ContainerCarrinho>
         <div>
           <h3>Carrinho:</h3>
         </div>
-        {/* <div>
-          {this.state.cestaDeProdutos.map(props => {
-            return <Lista
-              key={props.id}
-              nome={props}
-              Adiciona={this.adicionaAoCarrinho()}
-              Remove={this.removeDoCarrinho()}
-            />
-          })}
-        </div> */}
+       
         <div>
-          <button onClick={() => this.props.removeDoCarrinho()}>Remover</button>
+          {/* <button onClick={() => this.props.removeDoCarrinho()}>Remover</button> */}
         </div>
         Valor total: R$
       </ContainerCarrinho>
